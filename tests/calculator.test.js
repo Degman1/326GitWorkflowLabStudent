8113a1141198a699dafa48f09b6a266287e2bf95
Asritha <asrithab2022@gmail.com>
2024-10-16 09:46:00 -0400

    updating

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,15 +2,15 @@ const assert = require('assert');
 const calculator = require('../calculator');
 
 // Test memory functions
-describe('Calculator Memory Functions', function() {
-    
+describe('Calculator Memory Functions', function () {
+
 });
 
 
 // Test calculation functions
-describe('Calculator Calculation Functions', function() {
+describe('Calculator Calculation Functions', function () {
     const res = calculator.calculateSquareRoot(4);
-    it('should return 2', function() {
+    it('should return 2', function () {
         assert.strictEqual(res, 2);
     });
 });
